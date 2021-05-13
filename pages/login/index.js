afce6d784c7efb8c51385868c7d1216009dde378
Gonzalo Esperidioni <gonzaloesperidioni@gmail.com>
2021-05-12 21:10:28 -0300

    agrego pagina de login demo

---
+++ pages/login/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+    Flex,
+    Box,
+    Heading,
+    FormControl,
+    FormLabel,
+    Input,
+    Button,
+    Center
+} from '@chakra-ui/react';
+
+export default function LoginForm() {
+    return (
+        <Center>
+        <Flex width="full" align="center" justifyContent="center">
+            <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
+                <Box textAlign="center">
+                    <Heading>Login</Heading>
+                </Box>
+                <Box my={4} textAlign="left">
+                    <form>
+                        <FormControl>
+                            <FormLabel>Email</FormLabel>
+                            <Input type="email" placeholder="test@test.com" />
+                        </FormControl>
+                        <FormControl mt={6}>
+                            <FormLabel>Password</FormLabel>
+                            <Input type="password" placeholder="*******" />
+                        </FormControl>
+                        <Button width="full" mt={4} type="submit">
+                            Sign In
+              </Button>
+                    </form>
+                </Box>
+            </Box>
+        </Flex>
+        </Center>
+    );
+}
