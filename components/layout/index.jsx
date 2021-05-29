3e00b11fac8e451122528cdf04404fdf9727827a
Gonzalo Esperidioni <gonzaloesperidioni@gmail.com>
2021-05-29 20:50:34 -0300

    trivia battle

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -2,9 +2,9 @@ import { Grid, GridItem } from "@chakra-ui/react"
 import Header from "/components/header"
 import Footer from "/components/footer"
 export default function Layout({ children }) {
-    return <Grid minHeight="100vh" autoRows>
-        <GridItem><Header /></GridItem>
-        <GridItem justifyItems="stretch">{children}</GridItem>
+    return <Grid minHeight="100vh" templateRows="fit-content(100%) 1fr fit-content(100%)">
+        <GridItem ><Header /></GridItem>
+        <GridItem justifyItems="stretch" p="4">{children}</GridItem>
         <GridItem alignSelf="end"><Footer /></GridItem>
     </Grid>
 }
