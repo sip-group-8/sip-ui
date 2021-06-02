b48239ed0a6a2fa790273a05ae475f68dfdcab53
Diego Gabriel Lopez <diego9966@gmail.com>
2021-06-02 18:09:15 -0300

    'dgl'

--- components/productCard/index.jsx
+++ components/productCard/index.jsx
@@ -14,7 +14,7 @@ import {
 const IMAGE =
     'https://images.unsplash.com/photo-1585338107529-13afc5f02586?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80';
 
-export default function ProductCard({ picture = IMAGE, playedTimes, title, playUrl}) {
+export default function ProductCard({ picture, playedTimes, title, playUrl}) {
     return (
         <Center py={12}>
             <Box
@@ -55,16 +55,16 @@ export default function ProductCard({ picture = IMAGE, playedTimes, title, playU
                         height={230}
                         width={282}
                         objectFit={'cover'}
-                        src={IMAGE}
+                        src={picture}
                     />
                 </Box>
                 <Stack pt={10} align={'center'}>
                     <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
                         {playedTimes}
-            </Text>
-                    <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                        {title}
-            </Heading>
+                    </Text>
+                    <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500} color={"black"}>
+                       <strong>{title}</strong> 
+                    </Heading>
                     <Stack direction={'row'} align={'center'}>
                         <Text fontWeight={800} fontSize={'xl'}>
                             <Link href={playUrl}>
--- pages/games/index.jsx
+++ pages/games/index.jsx
@@ -16,9 +16,9 @@ export default function GamesPage() {
     return (
         <Flex align="center" justifyContent="center">
             <Stack direction="row" spacing="24" >
-                <ProductCard title="Crucigrama" playedTimes="123" playUrl="/game/crucigrama" />
-                <ProductCard title="Ta te ti" playedTimes="321" playUrl="/game/tateti" />
-                <ProductCard title="Quiz Battle" playedTimes="123" playUrl="/game/quiz" />
+                <ProductCard title="Crucigrama" playedTimes="123" playUrl="/game/crucigrama" picture='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvzQW3bGRT51oAxy3tgRGpG2t0OMNbeoBugA&usqp=CAU' />
+                <ProductCard title="Ta Te Ti" playedTimes="321" playUrl="/game/tateti" picture="https://www.purochamuyo.com/wp-content/uploads/2020/08/tateti.png" />
+                <ProductCard title="Quiz Battle" playedTimes="123" playUrl="/game/quiz" picture="https://generacionxbox.com/wp-content/uploads/2018/01/its-quiz-time-analisis.jpg" />
             </Stack>
         </Flex>
     );
